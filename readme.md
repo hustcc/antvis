d90b85e60c6f27ce05f317f3470f98abe7732a8c
hustcc <i@hust.cc>
2024-03-06 19:43:29 +0800

    chore: init

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Dashboard
+
+![image](https://github.com/hustcc/antvis/assets/7856674/3753b417-3905-410e-8b92-f60d9fc20f70)
+
+
+## License
+
+MIT@[hustcc](https://github.com/hustcc).
---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from streamlit_g2 import g2
+
+st.set_page_config(page_title="Dashboard for AntV!", page_icon="📈")
+
+"""
+## Welcome to Dashboard for [AntV](https://github.com/antvis) Open Source Group!
+"""
+
+st.image("https://github.com/hustcc/antvis/assets/7856674/3753b417-3905-410e-8b92-f60d9fc20f70")
